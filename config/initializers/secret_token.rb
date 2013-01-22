4fc59ed352e278e1a0339b0da0e553a048ad6c95
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2013-01-21 17:21:37 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RackInput::Application.config.secret_key_base = 'fd63d5c97751b13080410bea728a298f6b50c7389bf5606c259818ae574849a7734eff130fc18cbc35d59535518dfdc6b4c88aedafa9f334e596ab17382ece7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RackInput::Application.config.session_store :encrypted_cookie_store, key: '_rack_input_session'
